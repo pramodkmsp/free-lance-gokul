4cdd0937687460bb90b4c7d4c2b2a9dca0e0d80c
pramodkmsp <pramodkmsp1995@gmail.com>
2021-06-16 17:21:47 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+import { ChartsModule } from 'ng2-charts';
+import { Chart1Component } from './components/charts/chart1/chart1.component';
+import { Chart2Component } from './components/charts/chart2/chart2.component';
+import { Chart3Component } from './components/charts/chart3/chart3.component';
+import { Chart4Component } from './components/charts/chart4/chart4.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, DashboardComponent, Chart1Component, Chart2Component, Chart3Component, Chart4Component],
+  imports: [BrowserModule, AppRoutingModule, ChartsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
